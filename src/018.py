6224586921b14988eefea7ef55899ce2b711d1a1
Erik Båvenstrand <erik.bavenstrands@gmail.com>
2018-07-30 23:28:10 +0200

    018 complete

--- src/018.py
+++ src/018.py
@@ -1,3 +1,22 @@
-inputFile = open(r'C:/Users/Erik/Documents/Projekt/Programming/Project-Euler/src/resources/018.txt', 'r')
+inputFile = open(r'./resources/018.txt', 'r')
 triangleList = [[]]
-tempRow = inputFile.
+for line in inputFile.readlines():
+    triangleList.append(line.split(" "))
+triangleList.remove([])
+triangleList.reverse()
+upper = 0
+lower = 0
+line = 0
+while line < len(triangleList):
+    while upper + 1 < len(triangleList[line]):
+        if int(triangleList[line][upper]) + int(triangleList[line + 1][lower]) < int(triangleList[line][upper + 1]) + int(triangleList[line + 1][lower]):
+            triangleList[line + 1][lower] = int(triangleList[line][upper + 1]) + int(triangleList[line + 1][lower])
+        else:
+            triangleList[line + 1][lower] = int(triangleList[line][upper]) + int(triangleList[line + 1][lower])
+        upper += 1
+        lower += 1
+    line += 1
+    upper = 0
+    lower = 0
+triangleList.reverse()
+print "The maximum sum is ", triangleList[0][0]
